4e9dfb79643f903a34b5f0d0815169e495fe3cde
JonaBeqiri <jona.beqiri@student.uni-pr.edu>
2026-04-17 22:20:20 +0200

    Update VigenereCipher.py

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -32,6 +32,54 @@ def encrypt():
         else:
             result += text[i]
 
+
+
+    return result 
+
+if not key.isalpha():
+    print("Error - Key duhet te permbaje vetem shrkonja dhe te mos jete bosh!")
+else:
+    print("Encrypted text: ", encrypt())
+
+
+
+
+
+
+text = input("Enter text to decrypt: ")
+key =input("Enter key: ") 
+
+key_len = len(key)
+
+def decrypt():
+
+    result= ""
+    key_index = 0
+
+    for i in range(len(text)):
+        if text[i].isalpha():
+            if text[i].islower():
+                c=ord(text[i]) - ord('a')
+                k=ord(key[key_index % key_len].lower()) - ord('a')
+                p=(c - k + 26) % 26
+                result +=chr(p+ ord('a'))
+            else:
+                c=ord(text[i]) - ord('A')
+                k=ord(key[key_index % key_len].upper()) - ord('A')
+                p=(c - k + 26) % 26
+                result +=chr(p+ ord('A'))
+
+            key_index += 1
+        else:
+            result += text[i]
+
     return result
 
-print(encrypt())
+if not key.isalpha():
+    print("Error - Key duhet te permbaje vetem shrkonja dhe te mos jete bosh!")
+else:
+    print("Decrypted text: ", decrypt())
+
+
+
+
