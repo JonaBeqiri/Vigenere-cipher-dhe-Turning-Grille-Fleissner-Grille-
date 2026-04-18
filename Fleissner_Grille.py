f64e21d37aa845e0f3eabe5858e32417e50e86a6
Artin-mehana1 <artin.mehana@student.uni-pr.edu>
2026-04-18 16:30:25 +0200

    Update Fleissner_Grille.py

--- Fleissner_Grille.py
+++ Fleissner_Grille.py
@@ -1,1 +1,8 @@
 // Enkriptimi me Algoritmin Turning Grille
+
+def rotate_grille(grille):
+    n = len(grille)
+    return [[grille[n - j - 1][i] for j in range(n)] for i in range(n)]
+
+def encrypt_turning_grille(message, grille):
+    n = len(grille)
