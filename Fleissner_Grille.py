c56e7d0e1204508efc3fc396e8798b1ff0d70a67
Jon Vesel Gashi <jonvesel.gashi@student.uni-pr.edu>
2026-04-18 18:48:42 +0200

    if else per zgjedhje te programit
    shtohet if else per te zgjedhur enkriptim apo dekriptim

--- Fleissner_Grille.py
+++ Fleissner_Grille.py
@@ -1,116 +1,124 @@
-# Enkriptimi me Algoritmin Turning Grille
+zgjedhja = input("Zgjidhni veprimin (1 për enkriptim, 2 për dekriptim): ")
 
-def rotate_grille(grille):
-    n = len(grille)
-    return [[grille[n - j - 1][i] for j in range(n)] for i in range(n)]
-
-def encrypt_turning_grille(message, grille):
-    n = len(grille)
-
- # mbush mesazhin me X nëse është më i shkurtë
-    while len(message) < n * n:
-        message += 'X'
+if zgjedhja == '1':
     
-    matrix = [['' for _ in range(n)] for _ in range(n)]
-    index = 0
+    # Enkriptimi me Algoritmin Turning Grille
 
-    for _ in range(4):  # 4 rrotullime
-        for i in range(n):
-            for j in range(n):
-                if grille[i][j] == 1:
-                    matrix[i][j] = message[index]
+    def rotate_grille(grille):
+        n = len(grille)
+        return [[grille[n - j - 1][i] for j in range(n)] for i in range(n)]
+
+    def encrypt_turning_grille(message, grille):
+        n = len(grille)
+
+    # mbush mesazhin me X nëse është më i shkurtë
+        while len(message) < n * n:
+            message += 'X'
+        
+        matrix = [['' for _ in range(n)] for _ in range(n)]
+        index = 0
+
+        for _ in range(4):  # 4 rrotullime
+            for i in range(n):
+                for j in range(n):
+                    if grille[i][j] == 1:
+                        matrix[i][j] = message[index]
+                        index += 1
+            grille = rotate_grille(grille)
+
+    # kthe matricën në string
+        encrypted = ''.join([''.join(row) for row in matrix])
+        return encrypted
+
+
+    # Shembull përdorimi
+    grille = [
+        [1, 0, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 0],
+        [0, 1, 0, 0]
+    ]
+
+    message = "HELLOWORLD"
+
+    encrypted_text = encrypt_turning_grille(message, grille)
+    print("Mesazhi i enkriptuar:", encrypted_text)
+
+elif zgjedhja == '2':
+
+    #Dekriptimi me Algoritmin Turning Grille
+
+    def createGrid(size):
+        return [['' for _ in range(size)] for _ in range(size)]
+
+
+    def fillGrid(grid, text):
+        index = 0
+        size = len(grid)
+
+        for i in range(size):
+            for j in range(size):
+                if index < len(text):
+                    grid[i][j] = text[index]
                     index += 1
-        grille = rotate_grille(grille)
+                else:
+                    grid[i][j] = 'X'
 
-# kthe matricën në string
-    encrypted = ''.join([''.join(row) for row in matrix])
-    return encrypted
+    def rotateGrille(grille):
+        size = len(grille)
+        new_grille = [[0 for _ in range(size)] for _ in range(size)]
 
+        for i in range(size):
+            for j in range(size):
+                new_grille[j][size - 1 - i] = grille[i][j]
 
-# Shembull përdorimi
-grille = [
-    [1, 0, 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 0],
-    [0, 1, 0, 0]
-]
-
-message = "HELLOWORLD"
-
-encrypted_text = encrypt_turning_grille(message, grille)
-print("Mesazhi i enkriptuar:", encrypted_text)
-
-#Dekriptimi me Algoritmin Turning Grille
-
-def createGrid(size):
-    return [['' for _ in range(size)] for _ in range(size)]
+        return new_grille
 
 
-def fillGrid(grid, text):
-    index = 0
-    size = len(grid)
+    def readGrille(grid, grille):
+        size = len(grid)
+        result = ""
 
+        for i in range(size):
+            for j in range(size):
+                if grille[i][j] == 1:
+                    result += grid[i][j]
+
+        return result
+
+
+    def decrypt(ciphertext, grille):
+        size = len(grille)
+
+        grid = createGrid(size)
+        fillGrid(grid, ciphertext)
+
+        result = ""
+
+        current_grille = grille
+
+        for _ in range(4):
+            result += readGrille(grid, current_grille)
+            current_grille = rotateGrille(current_grille)
+
+        return result
+
+    ciphertext = input("Jep tekstin e enkriptuar: ").replace(" ", "")
+
+    length = int(input("Jep gjatësinë e mesazhit origjinal: "))   
+
+    size = int(input("Jep dimensionin e matricës (p.sh., 4 për 4x4): "))
+
+    print("Shkruani matricen rresht pas rreshti (1 për vrimë, 0 për pa vrimë):")
+
+    grille = []
     for i in range(size):
-        for j in range(size):
-            if index < len(text):
-                grid[i][j] = text[index]
-                index += 1
-            else:
-                grid[i][j] = 'X'
+        row = list(map(int, input(f"Row {i+1}: ").split()))
+        grille.append(row)
 
-def rotateGrille(grille):
-    size = len(grille)
-    new_grille = [[0 for _ in range(size)] for _ in range(size)]
+    plaintext = decrypt(ciphertext, grille)
 
-    for i in range(size):
-        for j in range(size):
-            new_grille[j][size - 1 - i] = grille[i][j]
-
-    return new_grille
-
-
-def readGrille(grid, grille):
-    size = len(grid)
-    result = ""
-
-    for i in range(size):
-        for j in range(size):
-            if grille[i][j] == 1:
-                result += grid[i][j]
-
-    return result
-
-
-def decrypt(ciphertext, grille):
-    size = len(grille)
-
-    grid = createGrid(size)
-    fillGrid(grid, ciphertext)
-
-    result = ""
-
-    current_grille = grille
-
-    for _ in range(4):
-        result += readGrille(grid, current_grille)
-        current_grille = rotateGrille(current_grille)
-
-    return result
-
-ciphertext = input("Jep tekstin e enkriptuar: ").replace(" ", "")
-
-length = int(input("Jep gjatësinë e mesazhit origjinal: "))   
-
-size = int(input("Jep dimensionin e matricës (p.sh., 4 për 4x4): "))
-
-print("Shkruani matricen rresht pas rreshti (1 për vrimë, 0 për pa vrimë):")
-
-grille = []
-for i in range(size):
-    row = list(map(int, input(f"Row {i+1}: ").split()))
-    grille.append(row)
-
-plaintext = decrypt(ciphertext, grille)
-
-print("\nMesazhi i dekriptuar:")
-print(plaintext[:length])
+    print("\nMesazhi i dekriptuar:")
+    print(plaintext[:length])
+else:
+    print("Zgjidhje e pavlefshme. Ju lutemi zgjidhni 1 për enkriptim ose 2 për dekriptim.")
